641f85ca5229bb486ec1f59d74b6a2df5fb6327b
Wojciech Semik <wojtek.semik@gmail.com>
2021-11-13 20:39:51 +0100

    Kind of a working.

--- main.py
+++ main.py
@@ -1,32 +1,50 @@
 import os
 import sys
 
-random_theme = os.getenv("RANDOM_THEME", "None")
-random_theme = os.getenv("VTE_VERSION", "None")
-
 # Extend section ZSH_THEME_RANDOM_IGNORED in the .zshrc file with theme name
 def add_ignored_themes_to_zshrc(ignored_theme_names):
     # remove duplicates from ignored_theme_names list
-    ignored_theme_names = list(dict.fromkeys(ignored_theme_names))
-
+    ignored_theme_names = sorted(list(set(ignored_theme_names)))
+    in_ignored_themes = False
     zshrc_path = os.path.expanduser("~/.zshrc_bak")
     with open(zshrc_path, "r") as zshrc_file:
         zshrc_lines = zshrc_file.readlines()
+
+    new_line = "ZSH_THEME_RANDOM_IGNORED=(" + " ".join(ignored_theme_names) + ")\n"
+    zshrc_new_lines = []
+
+    for line in zshrc_lines:
+        if "ZSH_THEME_RANDOM_IGNORED" in line:
+            if not ")" in line:
+                in_ignored_themes = True
+            zshrc_new_lines.append(new_line)
+        elif in_ignored_themes:
+            if ")" in line:
+                in_ignored_themes = False
+        else:
+            zshrc_new_lines.append(line)
     with open(zshrc_path, "w") as zshrc_file:
-        for line in zshrc_lines:
-            print(line)
-            if "ZSH_THEME_RANDOM_IGNORED" in line:
-                line = line.split("=")[0] + "=(" + " ".join(ignored_theme_names) + ")"
-            zshrc_file.write(line)
+        zshrc_file.write("".join(zshrc_new_lines))
 
 
 # Get items from ZSH_THEME_RANDOM_IGNORED section from the .zshrc file
 def get_ignored_themes_from_zshrc():
     zshrc_path = os.path.expanduser("~/.zshrc_bak")
+    ignored_themes = []
+    in_ignored_themes = False
     with open(zshrc_path, "r") as zshrc_file:
         for line in zshrc_file:
             if "ZSH_THEME_RANDOM_IGNORED" in line:
-                return line.split("=")[1].split("(")[1].split(")")[0].split(" ")
+                if ")" in line:
+                    return line.split("=")[1].split("(")[1].split(")")[0].split(" ")
+                else:
+                    in_ignored_themes = True
+            elif in_ignored_themes:
+                if ")" in line:
+                    in_ignored_themes = False
+                else:
+                    ignored_themes.append(line.strip())
+    return ignored_themes
 
 
 def main():
@@ -35,9 +53,7 @@ def main():
 
     current = get_ignored_themes_from_zshrc()
     current.append(random_theme)
-    print(current)
     add_ignored_themes_to_zshrc(current)
-    print(get_ignored_themes_from_zshrc())
 
 
 if __name__ == "__main__":
