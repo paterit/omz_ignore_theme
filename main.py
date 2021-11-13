d18d35b0fe6b28fd45b107cfcfdcc73f1e55555f
Wojciech Semik <wojtek.semik@gmail.com>
2021-11-13 23:16:27 +0100

    Full regex.

--- main.py
+++ main.py
@@ -2,30 +2,21 @@ import os
 import sys
 import re
 
-# Extend section ZSH_THEME_RANDOM_IGNORED in the .zshrc file with theme name
+
+# Extend section ZSH_THEME_RANDOM_IGNORED in the .zshrc file with theme names
 def add_ignored_themes_to_zshrc(ignored_theme_names):
     # remove duplicates from ignored_theme_names list
     ignored_theme_names = sorted(list(set(ignored_theme_names)))
-    in_ignored_themes = False
     zshrc_path = os.path.expanduser("~/.zshrc_bak")
     with open(zshrc_path, "r") as zshrc_file:
-        zshrc_lines = zshrc_file.readlines()
+        all = zshrc_file.read()
 
-    new_line = "ZSH_THEME_RANDOM_IGNORED=(" + " ".join(ignored_theme_names) + ")\n"
-    zshrc_new_lines = []
+    p = re.compile("^ZSH_THEME_RANDOM_IGNORED=\(\s*((?:\w+\s*)+)\)$", re.MULTILINE)
+    new_line = "ZSH_THEME_RANDOM_IGNORED=(" + " ".join(ignored_theme_names) + ")"
+    new_all = p.sub(new_line, all)
 
-    for line in zshrc_lines:
-        if "ZSH_THEME_RANDOM_IGNORED" in line:
-            if not ")" in line:
-                in_ignored_themes = True
-            zshrc_new_lines.append(new_line)
-        elif in_ignored_themes:
-            if ")" in line:
-                in_ignored_themes = False
-        else:
-            zshrc_new_lines.append(line)
     with open(zshrc_path, "w") as zshrc_file:
-        zshrc_file.write("".join(zshrc_new_lines))
+        zshrc_file.write(new_all)
 
 
 def get_ignored_themes_from_zshrc():
@@ -44,7 +35,7 @@ def main():
     assert len(sys.argv) == 2, "Usage: main.py <theme_name>"
     random_theme = sys.argv[1]
 
-    current = get_ignored_themes_from_zshrc2()
+    current = get_ignored_themes_from_zshrc()
     current.append(random_theme)
     add_ignored_themes_to_zshrc(current)
 
