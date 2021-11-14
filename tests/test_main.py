bdaf8c847c85a0fe68cca38e9865bc9ffec6dd9a
Wojciech Semik <wojtek.semik@gmail.com>
2021-11-14 13:14:58 +0100

    Adding tests.

---
+++ tests/test_main.py
@@ -0,0 +1,72 @@
+from main import build_new_zshrc_content
+
+
+CONTENT_EMPTY_SECTION_BEFORE = """
+ZSH_THEME="random"
+ZSH_THEME_RANDOM_IGNORED=()
+ZSH_THEME_RANDOM_CANDIDATES=( "robbyrussell" "agnoster" )
+"""
+CONTENT_EMPTY_SECTION_AFTER = """
+ZSH_THEME="random"
+ZSH_THEME_RANDOM_IGNORED=(new_theme)
+ZSH_THEME_RANDOM_CANDIDATES=( "robbyrussell" "agnoster" )
+"""
+CONTENT_NO_SECTION_BEFORE = """
+ZSH_THEME="random"
+ZSH_THEME_RANDOM_CANDIDATES=( "robbyrussell" "agnoster" )
+"""
+CONTENT_NO_SECTION_AFTER = """
+ZSH_THEME="random"
+ZSH_THEME_RANDOM_CANDIDATES=( "robbyrussell" "agnoster" )
+
+ZSH_THEME_RANDOM_IGNORED=(new_theme)
+"""
+CONTENT_MULTI_LINE_BEFORE = """
+ZSH_THEME="random"
+ZSH_THEME_RANDOM_IGNORED=(
+  sd27
+  sd28
+  sd29
+)
+ZSH_THEME_RANDOM_CANDIDATES=( "robbyrussell" "agnoster" )
+"""
+CONTENT_MULTI_LINE_AFTER = """
+ZSH_THEME="random"
+ZSH_THEME_RANDOM_IGNORED=(new_theme sd27 sd28 sd29)
+ZSH_THEME_RANDOM_CANDIDATES=( "robbyrussell" "agnoster" )
+"""
+
+CONTENT_SINGLE_LINE_BEFORE = """
+ZSH_THEME="random"
+ZSH_THEME_RANDOM_IGNORED=(sd27 sd28 sd29)
+ZSH_THEME_RANDOM_CANDIDATES=( "robbyrussell" "agnoster" )
+"""
+CONTENT_SINGLE_LINE_AFTER = """
+ZSH_THEME="random"
+ZSH_THEME_RANDOM_IGNORED=(new_theme sd27 sd28 sd29)
+ZSH_THEME_RANDOM_CANDIDATES=( "robbyrussell" "agnoster" )
+"""
+
+
+def test_empty_section():
+    assert CONTENT_EMPTY_SECTION_AFTER == build_new_zshrc_content(
+        "new_theme", CONTENT_EMPTY_SECTION_BEFORE
+    )
+
+
+def test_multi_line():
+    assert CONTENT_MULTI_LINE_AFTER == build_new_zshrc_content(
+        "new_theme", CONTENT_MULTI_LINE_BEFORE
+    )
+
+
+def test_single_line():
+    assert CONTENT_SINGLE_LINE_AFTER == build_new_zshrc_content(
+        "new_theme", CONTENT_SINGLE_LINE_BEFORE
+    )
+
+
+def test_no_section():
+    assert CONTENT_NO_SECTION_AFTER == build_new_zshrc_content(
+        "new_theme", CONTENT_NO_SECTION_BEFORE
+    )
